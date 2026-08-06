474f324baa63160597201a0c505770db016a0cf3
nesterone <igornester@gmail.com>
2026-08-06 22:52:11 +0200

    chore: add info about schema

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ try {
   await app.listen({ port, host: "0.0.0.0" });
   console.log(`Server listening on http://localhost:${port}`);
   console.log(`API Docs  http://localhost:${port}/docs`);
+  console.log(`API Schema  http://localhost:${port}/docs/json`);
 } catch (err) {
   console.error(err);
   process.exit(1);
